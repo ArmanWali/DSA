1b660c4a303260a365dd473e1f23cc30d7384aff
ArmanWali <150829536+ArmanWali@users.noreply.github.com>
2024-05-26 23:15:42 +0500

    Add files via upload

---
+++ Swap.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+//swap function
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+    int x,y;
+    cout<<"Enter the first number: "<<endl;
+    cin>>x;
+    cout<<"Enter the Second number: "<<endl;
+    cin>>y;
+    cout<<"The numbers before swapping : "<<endl;
+    cout<<"x = "<<x<<endl;
+    cout<<"y = "<<y<<endl;
+    swap(x,y);
+    cout<<"The numbers after swapping : "<<endl;
+    cout<<"x = "<<x<<endl;
+    cout<<"y = "<<y<<endl;
+
+    return 0;
+}
